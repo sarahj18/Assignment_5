6c16b603a1fa4b6b2ca70cab6d8108c28fd7ac43
Sara Hunfjord <sarahj18@ru.is>
2020-09-03 15:02:03 +0000

    Algorithm written for sequence.py

--- sequence.py
+++ sequence.py
@@ -12,13 +12,22 @@
 # second takes value of the third
 # first number takes value of the second 
 
-first = 0
-second = 0
-third = 1
-
 n = int(input('Enter the length of the sequence: '))
 
+first = 1
+second = 2
+third = 3
+
 for i in range(0,n):
-    next_number = first + second + third
-    third = next_number, second = third, first = second
-    print(next_number, end=' ')
+    if i == 0:
+        print(first)
+    elif i == 1:
+        print(second)
+    elif i == 2:
+        print(third)
+    else:
+        next_number = first + second + third
+        first = second
+        second = third
+        third = next_number
+        print(next_number)
