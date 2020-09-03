67b9d2869be8768725f70d9f6be151746c821239
Sara Hunfjord <sarahj18@ru.is>
2020-09-03 13:57:13 +0000

    Algorithm written for max_int

---
+++ max_int.py
@@ -0,0 +1,13 @@
+# Ask for a number
+# Assign value 0 to variable max_int which is maximum input 
+# in beginning
+# If input is negative in the beginning we ask again for a new input
+# If input is larger than maximum input it becomes the new maximum input
+# If input is smaller than maximum input the maximum input stays the same
+# ask for another number
+# If the input is negative we stop and print out the maximum input value
+
+num_int = int(input=("Input a number: "))
+max_int = 0
+
+print("The maximum is: ", max_int)
