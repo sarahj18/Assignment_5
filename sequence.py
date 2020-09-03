a5603bcef4178f0b9a36bd98cb5b7fe22a49321d
Sara Hunfjord <sarahj18@ru.is>
2020-09-03 14:43:07 +0000

    Algorithm written for sequence.py

---
+++ sequence.py
@@ -0,0 +1,19 @@
+# first = 0
+# second = 0
+# third = 1
+# ask for sequence number
+# for-lykkja from 0 to sequence number 
+# prints out the sequence
+# algorithm is as such;
+# number is addition of the last three numbers 
+# 1, 2, 3, 6, 11, 20, 37, 68, 125.. m
+# new number is printed out called next number
+# third number than takes value of the next number
+# second takes value of the third
+# first number takes value of the second 
+
+first = 0
+second = 0
+third = 1
+
+n = int(input('Enter the length of the sequence: '))
